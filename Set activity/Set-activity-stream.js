90b003b40bc37bd7870908c989ac5d59d66ea940
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2022-06-21 17:13:04 +0200

    Update and rename Activity (stream).js to Set-activity-stream.js
    - Updated name Activity (stream).js -> Set-activity-stream.js 
    - Formatted code properly
    - Added "comments" section

--- Set activity/Set-activity-stream.js
+++ Set activity/Set-activity-stream.js
@@ -1,14 +1,13 @@
-
-
-
 bot.on('ready', () => {
   bot.user.setStatus('available')
   bot.user.setPresence({
-      game: {
-          name: 'YOUR TEXT',
-          type: "STREAMING",
-          // ONLY TWITCH LINK WORKS
+    game: {
+      name: 'YOUR TEXT',
+      type: "STREAMING",
           url: "LINK"
       }
   });
 });
+
+//Comments
+//Only links from Twitch channels are allowed
