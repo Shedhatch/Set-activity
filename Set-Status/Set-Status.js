0f945360f901f2c933943f6f4c8dc74f44cf289e
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2022-06-21 19:22:51 +0200

    Update Set-Status.js
    - Removed "console.log"
    - Added comments
    - Formatted code
    - Improved English on comments

--- Set-Status/Set-Status.js
+++ Set-Status/Set-Status.js
@@ -1,15 +1,9 @@
-
-
 bot.on ("ready",fonction => {
-  console.log('I am ready!');
-//YOU CAN CHANGE THE STATUT ("dnd") BY
-//online - user is online
-//offline - user is offline or invisible
-//idle - user is AFK
-//dnd - user is in Do not Disturb
-  bot.user.setStatus("dnd");
-  bot.user.setActivity("YOUR TEXT");
-      return (console.error);
-  });
+  bot.user.setStatus("placeholder_status");
+  bot.user.setActivity("placeholder_text");
+  return (console.error);
+});
 
- 
+//Comments
+//You can switch between 4 types of status (online -> online | idle -> idle | dnd -> dnd | offline -> offline)
+//Placeholders must be changed
