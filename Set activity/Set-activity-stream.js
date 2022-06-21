dcfaced8755e41a6a2ab867d0ab424264659a7bb
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2022-06-21 17:14:47 +0200

    Update Set-activity-stream.js
    - Improved English
    - Improved comments

--- Set activity/Set-activity-stream.js
+++ Set activity/Set-activity-stream.js
@@ -2,12 +2,13 @@ bot.on('ready', () => {
   bot.user.setStatus('available')
   bot.user.setPresence({
     game: {
-      name: 'YOUR TEXT',
+      name: 'placeholder_name',
       type: "STREAMING",
-          url: "LINK"
+          url: "placeholder_link"
       }
   });
 });
 
 //Comments
 //Only links from Twitch channels are allowed
+//Placeholders must be changed
