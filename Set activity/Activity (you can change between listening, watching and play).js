55d2b6e0913cabc8e19df452958b3fba702b4d8d
Adrien Varricchione <47208777+Shedhatch@users.noreply.github.com>
2022-06-21 18:46:07 +0200

    Update and rename Set activity/Activity (you can change between listening, watching and play).js to OtherActivities.js
    - Updated name to Set activity/Activity (you can change between listening, watching and playing).js -> OtherActivities.js
    - Updated code
    - Removed a forgotten console log
    - Added comments

--- Set activity/Activity (you can change between listening, watching and play).js
+++
@@ -1,9 +0,0 @@
-
-bot.on('ready', () => {
-  console.log("Activity OK")
-  //CHANGE {type: 2} in 
-  //1 FOR PLAYING
-  //2 FOR LISTENING
-  //3 FOR WATCHING
-  bot.user.setActivity("YOUR TEXT", {type: 3});
-});
